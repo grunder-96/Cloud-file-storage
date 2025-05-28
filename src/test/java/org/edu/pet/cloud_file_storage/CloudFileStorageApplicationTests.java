e8231805c01524e9ad3fca04d379d9467d757272
Grigory Taran <grunder-96@mail.ru>
2025-05-28 14:58:26 +0300

    init

---
+++ src/test/java/org/edu/pet/cloud_file_storage/CloudFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edu.pet.cloud_file_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
