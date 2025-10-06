4959b307039525be98c93e1a33cc0cc7380bac96
Grigory Taran <grunder-96@mail.ru>
2025-10-06 14:25:40 +0300

    refactor: now using custom UserDetails with id field

---
+++ src/main/java/org/edu/pet/cloud_file_storage/security/CustomUserDetails.java
@@ -0,0 +1,37 @@
+package org.edu.pet.cloud_file_storage.security;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.util.Assert;
+
+import java.util.Collection;
+
+public class CustomUserDetails extends User {
+
+    @Getter
+    private final Long id;
+
+    public CustomUserDetails(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        Assert.isTrue(id != null,"Cannot pass null or empty values to constructor");
+        this.id = id;
+    }
+
+    @JsonCreator
+    public CustomUserDetails(@JsonProperty("id") Long id,
+                             @JsonProperty("username") String username,
+                             @JsonProperty("password") String password,
+                             @JsonProperty("enabled") boolean enabled,
+                             @JsonProperty("accountNonExpired") boolean accountNonExpired,
+                             @JsonProperty("credentialsNonExpired") boolean credentialsNonExpired,
+                             @JsonProperty("accountNonLocked") boolean accountNonLocked,
+                             @JsonProperty("authorities") Collection<? extends GrantedAuthority> authorities) {
+
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        Assert.isTrue(id != null,"Cannot pass null or empty values to constructor");
+        this.id = id;
+    }
+}
--- src/main/java/org/edu/pet/cloud_file_storage/security/UserDetailsServiceImpl.java
+++ src/main/java/org/edu/pet/cloud_file_storage/security/UserDetailsServiceImpl.java
@@ -2,12 +2,13 @@ package org.edu.pet.cloud_file_storage.security;
 
 import lombok.RequiredArgsConstructor;
 import org.edu.pet.cloud_file_storage.service.UserService;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -20,11 +21,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) {
 
         return userService.findByUsernameIgnoreCase(username)
-                .map(u -> User.builder()
-                        .username(u.getUsername())
-                        .password(u.getPassword())
-                        .build()
-                )
+                .map(u -> new CustomUserDetails(
+                        u.getId(),
+                        u.getUsername(),
+                        u.getPassword(),
+                        Collections.emptyList()
+                ))
                 .orElseThrow(() -> new UsernameNotFoundException(username));
     }
 }
