539d717d3afec04156c381c762c58fdcc95bd00d
Grigory Taran <grunder-96@mail.ru>
2026-09-01 15:50:18 +0300

    refactor: externalize minio configuration to properties class

--- src/main/java/org/edu/pet/cloud_file_storage/configuration/MinioConfig.java
+++ src/main/java/org/edu/pet/cloud_file_storage/configuration/MinioConfig.java
@@ -1,26 +1,21 @@
 package org.edu.pet.cloud_file_storage.configuration;
 
 import io.minio.MinioClient;
-import org.springframework.beans.factory.annotation.Value;
+import org.edu.pet.cloud_file_storage.configuration.properties.MinioProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties({MinioProperties.class})
 public class MinioConfig {
 
-    @Value("${MINIO_ACCESS_KEY:minio-user}")
-    private String accessKey;
-    @Value("${MINIO_SECRET_KEY:miniousrpass}")
-    private String secretKey;
-    @Value("${MINIO_PORT:9000}")
-    private int port;
-
     @Bean
-    public MinioClient minioClient() {
+    public MinioClient minioClient(MinioProperties minioProperties) {
 
         return MinioClient.builder()
-                .endpoint("http://localhost:%d/".formatted(port))
-                .credentials(accessKey, secretKey)
+                .endpoint(minioProperties.url())
+                .credentials(minioProperties.accessKey(), minioProperties.secretKey())
                 .build();
     }
 }
