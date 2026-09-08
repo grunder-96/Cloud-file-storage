9d95142701bc874d1980a2f522a7abd8492bac45
Grigory Taran <grunder-96@mail.ru>
2026-09-08 09:19:24 +0300

    revert to using Value annotation in MinioConfig

--- src/main/java/org/edu/pet/cloud_file_storage/configuration/MinioConfig.java
+++ src/main/java/org/edu/pet/cloud_file_storage/configuration/MinioConfig.java
@@ -1,21 +1,21 @@
 package org.edu.pet.cloud_file_storage.configuration;
 
 import io.minio.MinioClient;
-import org.edu.pet.cloud_file_storage.configuration.properties.MinioProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({MinioProperties.class})
 public class MinioConfig {
 
     @Bean
-    public MinioClient minioClient(MinioProperties minioProperties) {
+    public MinioClient minioClient(@Value("minio.url") String url,
+                                   @Value("minio.accessKey") String accessKey,
+                                   @Value("minio.secretKey") String secretKey) {
 
         return MinioClient.builder()
-                .endpoint(minioProperties.url())
-                .credentials(minioProperties.accessKey(), minioProperties.secretKey())
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
                 .build();
     }
 }
